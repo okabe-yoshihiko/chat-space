88141ba73467c2cd5a13043ad374b4731c352939
okabe-yoshihiko <skr.ys@icloud.com>
2020-02-18 12:21:06 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,6 @@
 lock '3.12.0'
 set :application, 'chat-space'
 set :repo_url,  'git@github.com:okabe-yoshihiko/chat-space.git'
-set :linked_files, %w{ config/secrets.yml }
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
 set :rbenv_type, :user
@@ -13,6 +12,7 @@ set :ssh_options, auth_methods: ['publickey'],
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
 set :keep_releases, 5
+set :linked_files, %w{ config/secrets.yml }
 
 
 after 'deploy:publishing', 'deploy:restart'
